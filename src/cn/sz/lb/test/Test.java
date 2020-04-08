f55dbb7245d3639fec6503de99d75c063833a517
camkls <cam_kls@163.com>
2020-04-08 17:48:47 +0800

    spc

---
+++ src/cn/sz/lb/test/Test.java
@@ -0,0 +1,24 @@
+package cn.sz.lb.test;
+
+import java.util.Date;
+
+/**
+ * Test
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("你/好!");
+        StringBuffer sb = new StringBuffer();
+    }
+
+    public void show1(){
+        Date date = new Date();
+    }
+    public void show2(){
+        String str = "";
+        String s = new String();
+    }
+    public void show3(){
+
+    }
+}
